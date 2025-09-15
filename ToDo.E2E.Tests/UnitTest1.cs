c059d4a965be2f935899511c3e1050e6e9f160a5
PVNKsenpai <phamkhanh261103@gmail.com>
2025-09-15 17:25:54 +0700

    Tests: add ToDo.E2E.Tests with NUnit + Selenium E2E test

---
+++ ToDo.E2E.Tests/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ToDo.E2E.Tests
+{
+    public class ToDoCRUDTests
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://localhost:5137");
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void CreateToDo_ShouldDisplayInList()
+        {
+            driver.FindElement(By.LinkText("Create")).Click();
+            driver.FindElement(By.Id("Title")).SendKeys("Học kiểm thử");
+            driver.FindElement(By.Id("Description")).SendKeys("Ôn lại test case CRUD");
+            driver.FindElement(By.CssSelector("input[type='submit']")).Click();
+
+            Assert.IsTrue(driver.PageSource.Contains("Học kiểm thử"), "ToDo không hiển thị trong danh sách");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
